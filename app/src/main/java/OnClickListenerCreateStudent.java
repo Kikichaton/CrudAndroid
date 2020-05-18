91393466b47e84e1927844f21fafeb79d05bdb4c
Kikichaton <56584589+Kikichaton@users.noreply.github.com>
2020-05-18 11:54:25 +0200

    1.1

---
+++ app/src/main/java/OnClickListenerCreateStudent.java
@@ -0,0 +1,23 @@
+import android.view.View;
+
+public class OnClickListenerCreateStudent implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        Context context = view.getRootView().getContext();
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View formElementsView = inflater.inflate(R.layout.student_input_form, null, false);
+        final EditText editTextStudentFirstname = (EditText) formElementsView.findViewById(R.id.editTextStudentFirstname);
+        final EditText editTextStudentEmail = (EditText) formElementsView.findViewById(R.id.editTextStudentEmail);
+        new AlertDialog.Builder(context)
+                .setView(formElementsView)
+                .setTitle("Create Student")
+                .setPositiveButton("Add",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+
+                                dialog.cancel();
+                            }
+
+                        }).show();
+    }
+}
